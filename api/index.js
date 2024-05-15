edff25d77fa287841424db9d1c3a3576312a8e7e
EnzoPa70 <enzo.angular50@gmail.com>
2024-05-15 18:06:54 +0200

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => {
+    console.log("mongoDB e' connesso");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
