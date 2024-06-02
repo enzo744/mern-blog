2f20b56748fd057b04fa39a6b22a24705fc4fdbc
EnzoPa70 <enzo.angular50@gmail.com>
2024-06-02 20:22:34 +0200

    add sign out functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -17,6 +17,7 @@ import {
   deleteUserStart,
   deleteUserSuccess,
   deleteUserFailure,
+  signoutSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
@@ -150,6 +151,21 @@ export default function DashProfile() {
     }
   };
 
+  const handleSignout=async ()=>{
+    try {
+      const res=await fetch("/api/user/signout", {
+        method:"POST",
+      });
+      const data = await res.json();
+      if(!res.ok){
+        console.log(data.message);
+      }else{
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
@@ -228,7 +244,7 @@ export default function DashProfile() {
         <span onClick={() => setShowModal(true)} className="cursor-pointer">
           Delete Account
         </span>
-        <span className="cursor-pointer">Sign Out</span>
+        <span onClick={handleSignout} className="cursor-pointer">Sign Out</span>
       </div>
       {updateUserSuccess && (
         <Alert color="success" className="mt-5">
@@ -273,23 +289,3 @@ export default function DashProfile() {
   );
 }
 
-{
-  /* {showModal && (
-        <div className="fixed top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.5)] flex justify-center">
-          <div className="bg-white w-96 p-5 rounded-md">
-            <h1 className="text-center text-2xl font-semibold">
-              Are you shure you want delete your account?
-            </h1>
-            <div className="flex justify-between mt-5">
-              <button
-              onClick={()=>setShowModal(false)}
-              gradientDuoTone='purpleToBlue'
-              outline
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      )} */
-}
