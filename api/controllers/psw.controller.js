1d9520aac3f0ed35584386a89f60f4a19cf47ffd
EnzoPa70 <enzo.angular50@gmail.com>
2024-06-13 17:09:20 +0200

    creata get psws api route

--- api/controllers/psw.controller.js
+++ api/controllers/psw.controller.js
@@ -30,3 +30,47 @@ export const create = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getPsws = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const psws = await Psw.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.title && { title: req.query.title }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.pswId && { _id: req.query.pswId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { commento: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+      const totalPsws=await Psw.countDocuments();
+      const now=new Date();
+
+      const oneMonthAgo = new Date(
+        now.getFullYear(),
+        now.getMonth() - 1,
+        now.getDate()
+      );
+
+      const lastMonthPsws = await Psw.countDocuments({
+        createdAt: { $gte: oneMonthAgo },
+      });
+      res.status(200).json({
+        psws,
+        totalPsws,
+        lastMonthPsws,
+      });
+  } catch (error) {
+    next(error);
+  }
+};
